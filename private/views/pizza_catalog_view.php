00539ae498582466e74c51775946de0107f45716
Agustin Oria <agustin.oria@gmail.com>
2019-04-02 13:53:45 -0300

    Function for base html generation.

--- private/views/pizza_catalog_view.php
+++ private/views/pizza_catalog_view.php
@@ -9,6 +9,33 @@ class pizza_catalog_view{
     }
 
     public function generate_pizza_catalog(){
-    	print "<div> This is a test for the pizza catalog </div>";
+    	
+
+    	$html = $this->generate_html($pizza_catalog);
+    }
+
+    private function generate_html($inner_html){
+    	$head = $this->generate_head();
+        $body = $this->generate_body($inner_html);
+    	$html = '<!DOCTYPE html><html lang="es">'. $head. $body. '</html>';
+
+    	return $html;
+    }
+
+    private function generate_head(){
+        $links = '<script src="/js/pizza_catalog.js"></script>';
+        $links .= '<script src="/js/minAjax.js"></script>';
+        $links .= '<link rel="stylesheet" type="text/css" href="/css/pizza_catalog.css">';
+        $title = '<title>TORO\'S PIZZA</title>';
+        $meta = '<meta charset="utf-8"/>';
+        $head = '<head>'. $meta. $links. $title. '</head>';
+
+        return $head;
+    }
+
+    private function generate_body($inner_html){
+        $body = '<body>'. $inner_html. '</body>';
+
+        return $body;
     }
 }
